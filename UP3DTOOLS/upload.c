9af2aa0625df1f1e273d6241326507479f71ef6a
Maik Stohn <git@stohn.de>
2016-02-26 21:42:25 +0100

    working program upload to SD :-)

--- UP3DTOOLS/upload.c
+++ UP3DTOOLS/upload.c
@@ -7,8 +7,12 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
-
+#include <unistd.h>
+#include <sys/time.h>
+    
 #include "up3d.h"
+
+#define upl_error(s) { printf("ERROR: %s\n",s); }
 
 int main(int argc, char const *argv[])
 {
@@ -27,47 +31,112 @@ int main(int argc, char const *argv[])
 
   if( !UP3D_IsPrinterResponsive() )
   {
-    printf( "UP printer is not responding\n" );
+    upl_error( "UP printer is not responding\n" );
     UP3D_Close();
     return 3;
   }
   
-  UP3D_SetParameter(PARA_RED_BLUE_BLINK,50);
+  UP3D_SetParameter(PARA_RED_BLUE_BLINK,10);
 
-/*
-  UP3D_BLK blk;
-  UP3D_BeginWrite();
-  UP3D_PROG_BLK_Power(&blk,true);UP3D_WriteBlock(&blk);
-  UP3D_PROG_BLK_Stop(&blk);UP3D_WriteBlock(&blk);
-  UP3D_Execute();
-*/
-  if( !UP3D_BeginWrite() )
+  UP3D_SetParameter(PARA_PRINT_STATUS,0x13); //write to SD
+
+  if( !UP3D_ClearProgramBuf() )
+  {
+    upl_error( "ClearProgramBuf failed\n" );
+    UP3D_Close();
     return 4;
+  }
+    
+  uint8_t program_id = 5;
 
-  if( !UP3D_SetProgramID( 7, true ) )
+//  UP3D_SetPrintJobInfo( program_id, 0, 1 );
+
+  if( !UP3D_UseSDProgramBuf( program_id, true ) )
+  {
+    upl_error( "UseSDProgramBuf failed\n" );
+    UP3D_Close();
     return 5;
+  }
 
+  long int fpos = 0;
+  fseek(fdat, 0, SEEK_END);
+  long int flen = ftell(fdat);
+  fseek(fdat, 0, SEEK_SET);
+
+  struct timeval tval_start, tval_now, tval_diff;
+  gettimeofday(&tval_start, NULL);
+  long int usecs = 1;
+
+  uint32_t tblocks = 0;
   for(;;)
   {
-    UP3D_BLK block;
-    if( sizeof(block) != fread( &block, 1, sizeof(block), fdat ) )
+    UP3D_BLK blocks[72];
+    int nblocks = fread( blocks, sizeof(UP3D_BLK), 72, fdat );
+    if( nblocks<=0 )
       break;
 
-    if( !UP3D_WriteBlocks( &block, 1 ) )
+    if( !UP3D_WriteBlocks( blocks, nblocks ) )
+    {
+      upl_error("Write to SD failed");
+      UP3D_Close();
       return 6;
+    }
 
-printf("Free: %d   \r", UP3D_GetFreeBlocks() );
+    tblocks += nblocks;
+
+    fpos = ftell(fdat);
+    gettimeofday(&tval_now, NULL);
+    timersub(&tval_now, &tval_start, &tval_diff);
+    usecs = (long int)tval_diff.tv_sec*1000000+(long int)tval_diff.tv_usec;
+
+    printf("UPload: %.2f kB sent (%ld%%) [%.2f kB/sec]\r",fpos/1024.0,fpos*100/flen, fpos*(1000000/1024.0)/usecs);
+    fflush(stdout);
   }
+  printf("\n");
 
   fclose( fdat );
 
-  if( !UP3D_BeginWrite() )
+  uint32_t w;
+  for( w=0; w<100; w++ )
+  {
+    if( UP3D_GetParameter(0xC6) == tblocks )
+      break;
+    usleep(50*1000); //wait a bit to let printer time to complete write to SD card
+  }
+  
+  if( UP3D_GetParameter(0xC6) != tblocks )
+  {
+    upl_error("Write to SD not finished");
+    UP3D_Close();
     return 7;
-  if( !UP3D_SetProgramID( 7, false ) )
-    return 8;
+  }
+  
+  UP3D_SetParameter(PARA_RED_BLUE_BLINK,0);
 
-  if( !UP3D_Execute() )
+  UP3D_SetParameter(PARA_PRINT_STATUS,0x1); //System ready
+
+//  UP3D_SetPrintJobInfo( program_id, 0, 0 );
+
+  if( !UP3D_ClearProgramBuf() )
+  {
+    upl_error( "ClearProgramBuf failed\n" );
+    UP3D_Close();
+    return 8;
+  }
+  
+  if( !UP3D_UseSDProgramBuf( program_id, false ) )
+  {
+    upl_error( "UseSDProgramBuf failed\n" );
+    UP3D_Close();
+    return 8;
+  }
+
+  if( !UP3D_StartResumeProgram() )
+  {
+    upl_error( "StartResumeProgram failed\n" );
+    UP3D_Close();
     return 9;
+  }
 
   UP3D_Close();
   return 0;
