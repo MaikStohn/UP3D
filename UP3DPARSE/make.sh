cc544f59224ae1911f4899aefa0e3c3ad1cfea54
Maik Stohn <git@stohn.de>
2016-03-01 19:13:49 +0100

    added compatibility for windows, macos, linux

--- UP3DPARSE/make.sh
+++ UP3DPARSE/make.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-gcc -O3 -o parse parse.c
+gcc -std=c99 -O3 -o parse parse.c
+gcc -std=c99 -O3 -o convg convg.c
+
+if [[ "$OSTYPE" == "msys" ]]; then
+strip parse.exe
+strip convg.exe
+else
 strip parse
-
-gcc -O3 -o convg convg.cpp
 strip convg
+fi
