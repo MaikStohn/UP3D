9b41dedb5222c35f495ea9bdd1534077dca12784
Maik Stohn <git@stohn.de>
2016-04-18 22:29:09 +0200

    fix detection of windows for winsock / netinet

--- UP3DCOMMON/compat.h
+++ UP3DCOMMON/compat.h
@@ -27,6 +27,9 @@
 
 #if (defined(_WIN16) || defined(_WIN32) || defined(_WIN64)) && !defined(__WINDOWS__)
 # define __WINDOWS__
+#endif
+
+#ifdef  __WINDOWS__
 # include <winsock2.h>
 #else
 # include <netinet/in.h>
